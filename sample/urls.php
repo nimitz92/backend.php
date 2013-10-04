d8dbbc63f659aef79157b7ddaa096ad787735232
Vibhaj Rajan <vibhaj8@gmail.com>
2013-10-04 13:55:59 +0530

    starting off with django-inspired smart url controller

--- sample/urls.php
+++ sample/urls.php
@@ -1,7 +1,7 @@
 <?php 
 	
 	$URLS = array(
-		'/^(conf|urls)$/i' => 'default.php',
+		'/^(urls|settings)$/i' => 'default.php',
 		'/^view\/(?P<user>[\w\-]+)\/(?P<id>[\d]+)$/i' => 'default.php',
 		'/^(?P<path>[\w\-\/]*)$/i' => BP_ROOT. 'include.php',
 	);
