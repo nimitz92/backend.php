c067dc117ae9bfeef98b13fd5fb6ab355e7aea7e
Vibhaj Rajan <vibhaj8@gmail.com>
2014-01-14 16:03:54 +0530

    added start-project script

--- start-project.sh
+++ start-project.sh
@@ -6,6 +6,7 @@ fi
 
 mkdir $1
 cd $1
+
 git init
 git submodule add http://github.com/tr4n2uil/backend.php.git lib/backend.php
 git submodule update --init --recursive
@@ -16,17 +17,20 @@ touch apps/models.php
 mkdir cache
 
 mkdir core
-cp lib/backend.php/demo/core/*.php core/*
+cp lib/backend.php/demo/core/hybridauth_config.php core/hybridauth_config.php
+cp lib/backend.php/demo/core/settings-final.php core/settings.php
+cp lib/backend.php/demo/core/urls.php core/urls.php
 
 mkdir tpl
-cp lib/backend.php/demo/tpl/*.jade tpl/*
+cp lib/backend.php/demo/tpl/*.jade tpl
 
 mkdir ui
 mkdir ui/style
-cp lib/backend.php/demo/ui/style/* ui/style/*
+cp lib/backend.php/demo/ui/style/* ui/style
 mkdir ui/script
 mkdir ui/image
 
 cp lib/backend.php/demo/.htaccess-final .htaccess
 cp lib/backend.php/demo/index.php index.php
 cp lib/backend.php/demo/build-styles.sh build-styles.sh
+
