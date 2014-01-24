3d5436c788c2dcedb7d0a735468a49474de6ce62
Vibhaj Rajan <vibhaj8@gmail.com>
2014-01-24 11:28:12 +0530

    added db backup

--- demo/core/settings-final.php
+++ demo/core/settings-final.php
@@ -36,12 +36,12 @@
 	// db utility settings
 	$DATABASES = array(
 		'default' => array( 
-			'dsn' => 'mysql:host=localhost;port=3306;dbname=thoughtsense',
+			'dsn' => 'mysql:host=localhost;port=3306;dbname=backend_php',
 			'user' => 'root',
 			'pass' => 'krishna',
 		),
 		'socket' => array(
-			'dsn' => 'mysql:unix_socket=/tmp/mysql.sock;dbname=blackpearl',
+			'dsn' => 'mysql:unix_socket=/tmp/mysql.sock;dbname=backend_php',
 			'user' => 'root',
 			'pass' => 'krishna',
 		),
